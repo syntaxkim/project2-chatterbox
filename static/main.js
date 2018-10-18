6f0ace180dadd751e87bd4a9520391b555bd0025
syntaxkim <lovems0219@gmail.com>
2018-10-18 23:21:28 +0900

    save user name inside sessionStorage (subject to change localStorage later)

--- static/main.js
+++ static/main.js
@@ -1,10 +1,36 @@
-document.addEventListner('DOMContentLoaded', () => {
+// Load initial name as 'name'
+if (!sessionStorage.getItem('name'))
+    sessionStorage.setItem('name', 'No Name');
 
-    // Connect to websocket
-    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+// When the DOM is done loaded,
+document.addEventListener('DOMContentLoaded', () => {
+    
+    // Set h1.innerHTML to session storage name value
+    document.querySelector('h1').innerHTML = sessionStorage.getItem('name');
 
-    // When connected,
-    socket.on('connect', () => {
-        socket.emit('event', {data: 'Hello'});
-    })
-})
+    // When a user joins with a new display name,
+    document.querySelector('#join').onsubmit = () => {
+        // Save user's name into name variable
+        let name = document.querySelector('#name').value;
+        // Set h1.innerHTML to newly joined user's display name
+        document.querySelector('h1').innerHTML = name;
+        // Keep user's name inside session storage
+        sessionStorage.setItem('name', name);
+    };
+
+    // When the user leaves,
+    document.querySelector('#leave').onclick = () => {
+        // clear the user's name
+        sessionStorage.clear();
+        // Reload the page
+        location.reload();
+    };
+});
+
+// Connect to websocket
+var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+// When connected,
+socket.on('connect', () => {
+    socket.emit('event', {data: 'Hello'});
+});
