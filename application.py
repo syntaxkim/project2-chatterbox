186bdd7eb2c6496d90602c1927f370d072ab809e
syntaxkim <lovems0219@gmail.com>
2018-10-18 20:46:43 +0900

    channel creation enabled

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -13,3 +13,10 @@ channel_list = ['general']
 @app.route("/")
 def index():
     return render_template("index.html", channels=channel_list)
+
+@app.route("/channel", methods=["POST"])
+def channel():
+    channel_name = request.form.get('channel')
+    channel_list.append(channel_name)
+    return redirect(url_for('index'))
+    
