800052c012c6bc94a0571412ce227ea68a41d056
syntaxkim <lovems0219@gmail.com>
2018-10-26 00:21:19 +0900

    functions added

--- static/index.js
+++ static/index.js
@@ -28,57 +28,13 @@ document.addEventListener('DOMContentLoaded', () => {
         socket.emit('load channel', {'before': null, 'after': channel});
         
         // Join in a user
-        document.querySelector('#join').onclick = () => {
-            const name = document.querySelector('#name').value;
-            if (!name) {
-                document.querySelector('#user_message').innerHTML = "No user name";
-                return false;
-            } else {
-                // Save user name in client-side memory
-                socket.emit('join', {'name': name}, data => {
-                    if (data === 1) {
-                        document.querySelector('#user_message').innerHTML = "Username already taken";
-                        return false;
-                    } else {
-                        sessionStorage.setItem('name', name);
-                        location.reload();
-                    };
-                });
-            };
-        };
+        document.querySelector('#join').onclick = join;
 
         // Send a message
-        document.querySelector('#send').onsubmit = () => {
-            const message = document.querySelector('#message').value;
-            if (!message) {
-                return false;
-            } else {
-                const channel = sessionStorage.getItem('channel');
-                socket.emit('send', {'channel': channel, 'name': name, 'message': message});
-                document.querySelector('#message').value = "";
-                // Do not reload
-                return false;
-            };
-        };
+        document.querySelector('#send').onsubmit = send;
 
         // Create a channel
-        document.querySelector('#create').onclick = () => {
-            const channel = document.querySelector('#channel_name').value;
-            if (!channel) {
-                document.querySelector('#channel_message').innerHTML = "No channel name";
-                return false;
-            } else {
-                socket.emit('create', {'channel': channel}, data => {
-                    if (data === 1) {
-                        document.querySelector('#channel_message').innerHTML = "The same channel name already exists.";
-                        return false;
-                    } else if (data === 0) {
-                        location.reload();
-                        alert(`New channel: '${channel}' created.`);
-                    };
-                });
-            };
-        };
+        document.querySelector('#create').onclick = create;
 
         // Load the channel
         document.querySelectorAll('.channel').forEach(link => {
@@ -102,6 +58,56 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Load messages
     socket.on('load messages', data => loadMessages(data));
+
+    function join() {
+        const name = document.querySelector('#name').value;
+        if (!name) {
+            document.querySelector('#user_message').innerHTML = "No user name";
+            return false;
+        } else {
+            // Save user name in client-side memory
+            socket.emit('join', {'name': name}, data => {
+                if (data === 1) {
+                    document.querySelector('#user_message').innerHTML = "Username already taken";
+                    return false;
+                } else {
+                    sessionStorage.setItem('name', name);
+                    location.reload();
+                };
+            });
+        };
+    };
+
+    function send() {
+        const message = document.querySelector('#message').value;
+        if (!message) {
+            return false;
+        } else {
+            const channel = sessionStorage.getItem('channel');
+            socket.emit('send', {'channel': channel, 'name': name, 'message': message});
+            document.querySelector('#message').value = "";
+            // Do not reload
+            return false;
+        };
+    };
+    
+    function create() {
+        const channel = document.querySelector('#channel_name').value;
+        if (!channel) {
+            document.querySelector('#channel_message').innerHTML = "No channel name";
+            return false;
+        } else {
+            socket.emit('create', {'channel': channel}, data => {
+                if (data === 1) {
+                    document.querySelector('#channel_message').innerHTML = "The same channel name already exists.";
+                    return false;
+                } else if (data === 0) {
+                    location.reload();
+                    alert(`New channel: '${channel}' created.`);
+                };
+            });
+        };
+    };
 
 });
 
